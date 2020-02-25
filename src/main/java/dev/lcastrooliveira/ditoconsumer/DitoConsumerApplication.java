4ec374d39b8e9c1bb9b2b29cdd95c07cfc895391
Lucas Oliveira <lcastro.oliveira@gmail.com>
2020-02-24 23:51:07 -0300

    Setup inicial

---
+++ src/main/java/dev/lcastrooliveira/ditoconsumer/DitoConsumerApplication.java
@@ -0,0 +1,13 @@
+package dev.lcastrooliveira.ditoconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DitoConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DitoConsumerApplication.class, args);
+    }
+
+}
